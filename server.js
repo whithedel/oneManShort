de693427cbd7054329779a1086bcc227a76c2b2b
Whithedel Carre <Whithedel@gmail.com>
2019-09-13 16:44:54 -0400

    added .env file to be able to use environement variables

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var options = {
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.DB_DATABASE
-};
+var options = process.env.JAWSDB_URL
+  ? mysql.createConnection(process.env.JAWSDB_URL)
+  : {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.DB_DATABASE
+    };
 
 var sessionStore = new MySQLStore(options);
 // Middleware
