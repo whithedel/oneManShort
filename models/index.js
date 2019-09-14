4d6c2595b1f553cecf1a34295d2b02680e43776a
Whithedel Carre <Whithedel@gmail.com>
2019-09-14 13:17:00 -0400

    remove connection to jawsdb on server.js

--- models/index.js
+++ models/index.js
@@ -9,7 +9,8 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log(process.env.JAWSDB_URL)
+  connection = new Sequelize(process.env.JAWSDB_URL);
 } else {
   var sequelize = new Sequelize(
     config.database,
