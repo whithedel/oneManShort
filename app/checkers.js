48853e6a3991489f5a198960f4287ee94f9b50fc
Keenan <keenan560@gmail.com>
2019-09-21 09:05:45 -0400

    has movable pieces

--- app/checkers.js
+++ app/checkers.js
@@ -30,12 +30,12 @@ var boardPiece = function (color, row, col, hasPiece, user, isKing) {
             // ************************* White Piece Logic **************************
             // if the current piece selected is White
             if (this.color === "White" && this.hasPiece === true) {
-                nextRow = this.row += 1;
+                nextRow = this.row + 1;
                 rightCol = this.col + 1;
                 leftCol = this.col - 1;
                 if (gameBoard[nextRow][rightCol] !== undefined && gameBoard[nextRow][rightCol].hasPiece === false) {
                     // if the square to the right is available then return that square's position
-                    moves.push([nextRow, rightCol]);
+                    moves.push(nextRow, rightCol);
                     // if the square to the right is occupied by another white piece
                 } if (gameBoard[nextRow][rightCol] !== undefined && gameBoard[nextRow][rightCol].hasPiece === true && gameBoard[nextRow][rightCol].color === "White") {
                     // Can't move
@@ -45,12 +45,12 @@ var boardPiece = function (color, row, col, hasPiece, user, isKing) {
                     // if the piece behind the black square is available
                     if (gameBoard[nextRow + 1][rightCol + 1].hasPiece === false) {
                         // then the white player can jump them.
-                        moves.push([nextRow + 1, rightCol + 1]);
+                        moves.push(nextRow + 1, rightCol + 1);
                         // else the piece behind the black piece is occupied
                     }
                 } if (gameBoard[nextRow][leftCol] !== undefined && gameBoard[nextRow][leftCol].hasPiece === false) {
                     // if the square to the left is available then return that square's position
-                    moves.push([nextRow, leftCol]);
+                    moves.push(nextRow, leftCol);
                     // if the square to the left is occupied by another white piece
                 } if (gameBoard[nextRow][leftCol] !== undefined && gameBoard[nextRow][leftCol].hasPiece === true && gameBoard[nextRow][leftCol].color === "White") {
                     // Can't move;
@@ -60,19 +60,19 @@ var boardPiece = function (color, row, col, hasPiece, user, isKing) {
                     // if the piece the behind the black square is available
                     if (gameBoard[nextRow + 1][leftCol + 1].hasPiece === false) {
                         // then the white player can jump them
-                        moves.push([nextRow + 1], [leftCol + 1]);
+                        moves.push(nextRow + 1, leftCol + 1);
                     }
                 }
                 // ************************* Black Piece Logic **************************
                 // if the current piece selected is black
             } else if (this.color === "Black" && this.hasPiece === true) {
-                nextRow = this.row -= 1;
+                nextRow = this.row - 1;
                 rightCol = this.col + 1;
                 leftCol = this.col - 1;
 
                 if (gameBoard[nextRow][rightCol] !== undefined && gameBoard[nextRow][rightCol].hasPiece === false) {
                     // if the square to the right is available
-                    moves.push([nextRow, rightCol]);
+                    moves.push(nextRow, rightCol);
                     // if the square to the right is another black piece
                 } if (gameBoard[nextRow][rightCol] !== undefined && gameBoard[nextRow][rightCol].hasPiece === true && gameBoard[nextRow][rightCol].color === "Black") {
 
@@ -84,13 +84,13 @@ var boardPiece = function (color, row, col, hasPiece, user, isKing) {
                     // if the piece behind the white square is available
                     if (gameBoard[nextRow + 1][rightCol + 1].hasPiece === false) {
                         // then the black player can jump them.
-                        moves.push([nextRow + 1, rightCol + 1]);
+                        moves.push(nextRow + 1, rightCol + 1);
                         // else the square behind the white piece is occupied
                     }
 
                 } if (gameBoard[nextRow][leftCol] !== undefined && gameBoard[nextRow][leftCol].hasPiece === false) {
                     // if the square to the left is available then return the position
-                    moves.push([nextRow, leftCol]);
+                    moves.push(nextRow, leftCol);
                     // if the square to the left is occupied by another black piece
                 } if (gameBoard[nextRow][leftCol] !== undefined && gameBoard[nextRow][leftCol].hasPiece === false && gameBoard[nextRow][leftCol].color === "Black") {
                     // Can't move
@@ -100,7 +100,7 @@ var boardPiece = function (color, row, col, hasPiece, user, isKing) {
                     // if the piece behind the white piece is available
                     if (gameBoard[nextRow + 1][leftCol + 1].hasPiece === false) {
                         // then the black player can jump them
-                        moves.push([nextRow + 1, leftCol + 1]);
+                        moves.push(nextRow + 1, leftCol + 1);
                         // else the square behind the white piece is occipied
                     }
                 }
@@ -216,13 +216,198 @@ let squaresCounter = function () {
 
 function renderGameBoard(gameBoard) {
     for (var i = 0; i < gameBoard.length; i++) {
+        var row = $("<tr>");
 
         for (var j = 0; j < gameBoard.length; j++) {
-            $("#gameboard").append(`<div class=>hello</div>`);
+
+            $(row).append(`<td draggable ='true' id='${i}${j}' class='tile ${gameBoard[i][j].color} text-center mx-3 'data-row='${i}' data-col='${j}'>${gameBoard[i][j].user}</td>`);
         }
+        $("tbody").append(row)
     }
+
+
 }
 
-console.log(gameBoard);
+// function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+// }
 
-$(document).ready(renderGameBoard(gameBoard))
+$(document).on("click", ".Black", (event) => {
+    event.preventDefault();
+    var currentId = event.target.id;
+    var row = $(`#${currentId}`).attr(`data-row`)
+    var col = $(`#${currentId}`).attr(`data-col`)
+    console.log(row, col);
+    console.log(this.window.gameBoard[row][col]);
+    var gamePiece = this.window.gameBoard[row][col];
+    var moves = gamePiece.possibleMoves();
+    console.log(moves)
+
+    highlightMoves(moves, gamePiece, gameBoard, currentId);
+
+
+
+})
+
+$(document).on("click", ".White", (event) => {
+    event.preventDefault();
+    var currentId = event.target.id;
+    var row = $(`#${currentId}`).attr(`data-row`)
+    var col = $(`#${currentId}`).attr(`data-col`)
+    console.log(row, col);
+    console.log(this.window.gameBoard[row][col]);
+    var gamePiece = this.window.gameBoard[row][col];
+    var moves = gamePiece.possibleMoves();
+    console.log(moves);
+
+    highlightMoves(moves, gamePiece, gameBoard, currentId);
+
+
+})
+
+$("#start-game").on("click", function (event) {
+    event.preventDefault();
+    renderGameBoard(gameBoard);
+    $("#tv").remove();
+    $(this).remove();
+
+
+})
+
+
+
+
+function highlightMoves(moves, gamePiece, gameBoard, currentId) {
+    // if there are no moves available
+    if (moves === undefined || moves.length === 0) {
+        alert("No moves for this piece");
+    }
+
+    // if there is only one move available
+    if (moves.length === 2) {
+        var row = moves[0].toString();
+        var col = moves[1].toString();
+        var id = row + col;
+        var move = gameBoard[row][col];
+        console.log(move);
+        $(`#${id}`).addClass("highlight");
+
+
+        $(document).on("click", `#${id}`, function (event) {
+            var newId = event.target.id;
+            $(this).removeClass("Square");
+            $(this).removeClass("highlight");
+            $(this).addClass(`${gamePiece.color}`);
+
+            var selectedPiece = gameBoard[newId[0]][newId[1]];
+            selectedPiece.color = gamePiece.color;
+            selectedPiece.hasPiece = true;
+            selectedPiece.user = gamePiece.user;
+            selectedPiece.isKing = false;
+
+            gamePiece.hasPiece = false;
+            gamePiece.color = "Square";
+            gamePiece.user = undefined;
+            moves = [];
+
+            $(`#${currentId}`).removeClass(`${gamePiece.color}`);
+            $(`#${currentId}`).addClass("Square");
+            $(`#${newId}`).html(`${selectedPiece.user}`);
+
+            // $("tbody").empty();
+            // renderGameBoard(gameBoard);
+        })
+
+    } if (moves.length > 2) {
+        var row1 = moves[0].toString();
+        var col1 = moves[1].toString();
+        var row2 = moves[2].toString();
+        var col2 = moves[3].toString();
+        var move1 = row1 + col1;
+        var move2 = row2 + col2;
+
+        $(`#${move1}`).removeClass("highlight");
+        $(`#${move2}`).removeClass("highlight");
+        $(`#${move1}`).addClass("highlight");
+        $(`#${move2}`).addClass("highlight");
+
+
+        $(document).on("click", `#${move1}`, function (event) {
+            var newId = event.target.id;
+            $(this).removeClass("Square");
+            $(this).removeClass("highlight");
+            $(this).addClass(`${gamePiece.color}`);
+            gamePiece.hasPiece = false;
+
+            // if I click on move1 then I update that object's color, haspiece, user, isKing, 
+            var selectedPiece = gameBoard[newId[0]][newId[1]];
+            selectedPiece.color = gamePiece.color;
+            selectedPiece.hasPiece = true;
+            selectedPiece.user = gamePiece.user;
+            selectedPiece.isKing = false;
+
+
+            var forsakenMove = gameBoard[row2][col2];
+            forsakenMove.color = "Square";
+            forsakenMove.user = undefined;
+            forsakenMove.hasPiece = false;
+            forsakenMove.isKing = false;
+            console.log(forsakenMove);
+
+            gamePiece.color = "Square";
+            gamePiece.user = undefined;
+            moves = [];
+
+            $(`#${currentId}`).removeClass("White");
+            $(`#${currentId}`).addClass("Square");
+            $(`#${newId}`).html(`${selectedPiece.user}`);
+            $(`#${move2}`).removeClass("highlight");
+            $(`#${move2}`).removeClass(`${gamePiece.color}`);
+            $(`#${move2}`).addClass("Square");
+            move1 = "";
+            move2 = "";
+            // $("tbody").remove();
+            // renderGameBoard(gameBoard);
+            
+
+
+        })
+
+        $(document).on("click", `#${move2}`, function (event) {
+            var newId = event.target.id;
+            $(this).removeClass("Square");
+            $(this).removeClass("highlight");
+            $(this).addClass(`${gamePiece.color}`);
+            gamePiece.hasPiece = false;
+            var selectedPiece = gameBoard[newId[0]][newId[1]];
+            selectedPiece.color = gamePiece.color;
+            selectedPiece.hasPiece = true;
+            selectedPiece.user = gamePiece.user;
+            selectedPiece.isKing = false;
+
+
+            var forsakenMove = gameBoard[row1][col1];
+            console.log(forsakenMove);
+            forsakenMove.color = "Square";
+            forsakenMove.user = undefined;
+            forsakenMove.hasPiece = false;
+            forsakenMove.isKing = false;
+            moves = [];
+
+
+            $(`#${currentId}`).removeClass(`${gamePiece.color}`);
+            $(`#${currentId}`).addClass("Square");
+            $(`#${newId}`).html(`${selectedPiece.user}`);
+            $(`#${move1}`).removeClass("highlight");
+            $(`#${move1}`).removeClass(`${gamePiece.color}`);
+            $(`#${move1}`).addClass("Square");
+            move1 = "";
+            move2 = "";
+
+        })
+
+
+
+
+    }
+}
